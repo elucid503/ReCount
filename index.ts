a583ae7260bc22262983005e848e6c346c295de0
Elucid503 <paulujlaky@gmail.com>
2024-01-08 18:23:25 -0500

    Add DB connection to index.ts and export it. Refactor to main()

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ import {
     ClientEvents
 } from "@projectdysnomia/dysnomia";
 
+import { MongoClient, ServerApiVersion } from "mongodb";
+
 import fs from "fs/promises";
 
 import APIKeys from "./configs/keys.json";
@@ -73,79 +75,111 @@ export class ReCount extends DysnomiaClient {
 
 }
 
-// Creating the Client
+// Connecting to the database
 
-const Client = new ReCount(APIKeys.DiscordToken, {
+const DBClient = new MongoClient(APIKeys.DatabaseURI, {
 
-    gateway: {
-
-        intents: [ Constants.Intents.allNonPrivileged, Constants.Intents.guildMessages ]
-
-    },
-
-    restMode: true
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
     
 });
+    
+await DBClient.connect().catch((error) => {
 
-// Init handlers
-
-const Files = { 
-
-    Commands: await fs.readdir("./commands"),
-    Events: await fs.readdir("./events"),
-
-}
-
-// Load handlers
-
-for (const file of Files.Commands) {
-
-    // Although we dont use Client.Commands in this file, it's good practice to load them immediately 
-
-    const Command = (await import(`./commands/${file}`)).default;
-
-    Client.Commands.set(Command.Name, Command);
-
-}
-
-for (const file of Files.Events) {
-
-    const Event = (await import(`./events/${file}`)).default;
-
-    Client.Events.set(Event.Name, Event);
-
-}
-
-// Hook Events
-
-for (const Event of Client.Events.values()) {
-
-    Client.on(Event.Name, Event.Run.bind(null, Client));
-
-}
-
-Client.connect(); // Connect to the gateway
-
-// Process handlers
-
-process.on("unhandledRejection", (error) => {
-
-    Log("Unhandled Rejection", `${error}`, LoggingColors.RED);
+    Log("Database Error", `Error connecting to database: ${error}`, LoggingColors.RED);
 
 });
 
-process.on("uncaughtException", (error) => {
+const DB = DBClient.db("ReCount");
 
-    Log("Uncaught Exception", `${error}`, LoggingColors.RED);
+async function Main() {
 
-});
+    // Creating the Client
 
-process.on("SIGINT", async () => {
+    const Client = new ReCount(APIKeys.DiscordToken, {
+
+        gateway: {
+
+            intents: [Constants.Intents.allNonPrivileged, Constants.Intents.guildMessages]
+
+        },
+
+        restMode: true
+    
+    });
+
+    // Init handlers
+
+    const Files = {
+
+        Commands: await fs.readdir("./commands"),
+        Events: await fs.readdir("./events"),
+
+    }
+
+    // Load handlers
+
+    for (const file of Files.Commands) {
+
+        // Although we dont use Client.Commands in this file, it's good practice to load them immediately 
+
+        const Command = (await import(`./commands/${file}`)).default;
+
+        Client.Commands.set(Command.Name, Command);
+
+    }
+
+    for (const file of Files.Events) {
+
+        const Event = (await import(`./events/${file}`)).default;
+
+        Client.Events.set(Event.Name, Event);
+
+    }
+
+    // Hook Events
+
+    for (const Event of Client.Events.values()) {
+
+        Client.on(Event.Name, Event.Run.bind(null, Client));
+
+    }
+
+    Client.connect(); // Connect to the gateway
+
+    // Process handlers
+
+    process.on("unhandledRejection", (error) => {
+
+        Log("Unhandled Rejection", `${error}`, LoggingColors.RED);
+
+    });
+
+    process.on("uncaughtException", (error) => {
+
+        Log("Uncaught Exception", `${error}`, LoggingColors.RED);
+
+    });
+
+    process.on("SIGINT", async () => {
  
-    Client.disconnect({ reconnect: false });
+        Client.disconnect({ reconnect: false });
     
-    Log("Process Exiting", "Disconnected from the Discord gateway.", LoggingColors.YELLOW);
+        Log("Process Exiting", "Disconnected from the Discord gateway.", LoggingColors.YELLOW);
 
-    process.exit(0);
+        process.exit(0);
     
-})
+    });
+
+}
+
+if (import.meta.url === `file://${process.argv[1]}`) {
+
+    Main();
+
+}
+
+export { DB }
