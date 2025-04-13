0b09c1714f696ca67bfd48ef21ad64c0ec69cc42
elucid503 <elucid@duck.com>
2025-04-13 17:34:49 -0400

    Update logging to use relay

--- extras/functions/logging.ts
+++ extras/functions/logging.ts
@@ -1,3 +1,5 @@
+import { LogLevel, Log as LogToRelay } from "sprout-api";
+
 import { LoggingColors, ResolvableColors } from "../types/logging";
 import { CreateEmbed } from "./embeds";
 
@@ -5,71 +7,74 @@ import { LoggingWebhook } from "../../configs/keys.json";
 
 export async function Log(subject: string, message: string, color: LoggingColors): Promise<boolean> {
     
-    // Get time and format it
+  // Get time and format it
 
-    const date: Date = new Date();
+  const date: Date = new Date();
 
-    const time: string = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    
-    const output: string = `${LoggingColors.CYAN}${time}${LoggingColors.RESET} • ${color}${subject}${LoggingColors.RESET}\n${message}`;
+  const time: string = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
   
-    console.log(output);
+  const output: string = `${LoggingColors.CYAN}${time}${LoggingColors.RESET} • ${color}${subject}${LoggingColors.RESET}\n${message}`;
+
+  console.log(output);
+
+  // Make a new message variable without any styling
+
+  const nostyle: string = message?.replace(/\[([0124578])m/g, "");
   
-    // Make a new message variable without any styling
+  const InferredLogLevel: LogLevel = (color === LoggingColors.RED) ? LogLevel.Error : (color === LoggingColors.YELLOW) ? LogLevel.Warning : LogLevel.Info;
+
+  try {
+
+    LogToRelay("W6QLnZW9UHJ2HH3p", InferredLogLevel, subject, message);
+
+    await PostToWebhook(subject, nostyle, null, ResolvableColors[color as keyof typeof ResolvableColors]);
+
+  } catch (error) {
+
+    console.error(`Error posting to webhook/relay: ${error}`);
+    return false;
+
+  }
+
+  return true;
+
+}
   
-    const nostyle: string = message?.replace(/\[([0124578])m/g, "");
+async function PostToWebhook(subject: string, message: string, author: string | null, color: ResolvableColors) {
   
-    try {
-  
-        await PostToWebhook(subject, nostyle, null, ResolvableColors[color as keyof typeof ResolvableColors]);
-  
-    } catch (error) {
-  
-        console.error(`Error posting to webhook: ${error}`);
-        return false;
-  
+  const embed = CreateEmbed({
+
+    title: subject,
+    description: message,
+    color: color,
+
+    author: {
+
+      name: author || "New Log Outputted",
+
     }
-  
-    return true;
-  
-  }
-  
-  async function PostToWebhook(subject: string, message: string, author: string | null, color: ResolvableColors) {
     
-    const embed = CreateEmbed({
-  
-      title: subject,
-      description: message,
-      color: color,
-  
-      author: {
-  
-        name: author || "New Log Outputted",
-  
-      }
-      
-    });
-  
-    fetch(LoggingWebhook, {
-  
-      method: "post",
-      headers: {
-  
-        "Content-Type": "application/json"
-  
-      },
-  
-      body: JSON.stringify({
-  
-        embeds: [embed]
-  
-      })
-  
-    }).catch((error) => { 
-  
-      console.error(`Error posting to webhook: ${error}`);
-  
-    });
-  
-  }
-  
+  });
+
+  fetch(LoggingWebhook, {
+
+    method: "post",
+    headers: {
+
+      "Content-Type": "application/json"
+
+    },
+
+    body: JSON.stringify({
+
+      embeds: [embed]
+
+    })
+
+  }).catch((error) => { 
+
+    console.error(`Error posting to webhook: ${error}`);
+
+  });
+
+}
